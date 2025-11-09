5f55a13b15f26c1dad897306feb55ad13c2bcc77
makaylax14 <mlee347@montgomerycollege.edu>
2025-11-09 17:23:59 -0500

    Added CheckingAccount.java

---
+++ CMSC203_Lab6/CheckingAccount.java
@@ -0,0 +1,12 @@
+
+public class CheckingAccount extends BankAccount{
+	private static final double FEE=.15;
+	public CheckingAccount(String name, double amount) {
+		super(name, amount);
+		setAccountNumber(getAccountNumber()+"-10");
+	}
+	@Override
+	public boolean withdraw(double amount) {
+		return super.withdraw(FEE+amount);
+	}
+}
